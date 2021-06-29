3fb643d0c458161fe06524b3c50a6ff7bd954fbd
Computador <dayna.caroline@unesp.br>
2021-06-29 11:54:05 -0300

    Add tabelas ponto

---
+++ database/migrations/2021_06_29_144928_create_pontos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePontosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pontos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('users')->unsigned();
+            $table->integer('motivo');
+            $table->foreign('users')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pontos');
+    }
+}
